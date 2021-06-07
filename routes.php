e572a1a69f2d1d0ee2dfe15eb3cc4e8c6489d18a
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-06-07 15:21:55 +0200

    Removed old code

--- routes.php
+++ routes.php
@@ -1,15 +1,5 @@
 <?php
 
-Route::get('/api/v1/auth/login', function () {
-    $user = \RainLab\User\Models\User::find(1);
-
-    $token = $user->createToken('Test token')->accessToken;
-
-    return [
-        'token' => $token,
-    ];
-});
-
 Route::get('/passport/test-route', function () {
     return 'You are successfully viewing a protected resource! Well done!';
 })->middleware('auth:api');
